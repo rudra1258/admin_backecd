d470a4df2c7e0d33a8a5fb8557e97ef63918c800
Banita Biswal <banitabiswal468@gmail.com>
2025-11-16 00:08:13 +0530

    user_create model

--- kg_app/admin.py
+++ kg_app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import admin_user_model
+from .models import *
 # Register your models here.
 
 admin.site.register(admin_user_model)
+
+@admin.register(CreateUser)
+class CreateUserAdmin(admin.ModelAdmin):
+    list_display = ("admin_id","id","first_name","last_name","email","phone_number","role","username","created_at")
+    search_fields = ("first_name","last_name","email","phone_number","username")
+    list_filter = ("role","created_at")
--- kg_app/models.py
+++ kg_app/models.py
@@ -11,3 +11,26 @@ class admin_user_model(models.Model):
     
     def __str__(self):
         return f"{self.admin_id} - {self.username}"
+    
+
+
+
+class CreateUser(models.Model):
+    ROLE_CHOICES = [
+        ('telecaller', 'Telecaller'),
+        ('teamlead', 'Team Lead'),
+        ('groundstaff', 'Ground Staff'),
+    ]
+    admin_id = models.ForeignKey(admin_user_model, on_delete=models.CASCADE,default=1)
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.role}"
